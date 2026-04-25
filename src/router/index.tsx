db052875109f1a368100b7d492f33faa79cb2d0c
xxxjinn <tlsguswls319@naver.com>
2026-04-26 03:41:04 +0900

    refactor: router 수정

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom';
 
 import { OnboardingPage } from '@/pages/OnboardingPage';
+import HomePage from '@/pages/HomePage';
 
 export const router = createBrowserRouter([
   {
     path: '/onboarding',
     element: <OnboardingPage />,
+  },
+  {
+    path: '/',
+    element: <HomePage />,
   },
 ]);
