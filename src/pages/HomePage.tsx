7e9b287c29352771b8211e2d9f3924db4e3b644d
hxezu <hailey_apple@icloud.com>
2026-04-26 02:59:47 +0900

    style : 타이틀 스타일 수정

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,16 +7,16 @@ import { PartyCard } from './home/PartyCard';
 function HomePage() {
   return (
     <MobileLayout>
-      <div className="flex min-h-screen flex-col bg-gradient-bg">
+      <div className="bg-gradient-bg flex min-h-screen flex-col">
         <HomeHeader />
         <div className="flex flex-col gap-2 px-4">
           <div className="flex flex-col gap-2.25 py-5">
-            <H2>
+            <H2 className="font-bold">
               오늘은 누구의 생일을
               <br />
               축하해볼까요?
             </H2>
-            <L1 className="text-grey-500">축하가 끝난 뒤에는 롤링페이퍼도 함께 보낼 수 있어요</L1>
+            <L1 className="text-grey-400">축하가 끝난 뒤에는 롤링페이퍼도 함께 보낼 수 있어요</L1>
           </div>
           <PartyCard />
           <ArchiveCard />
