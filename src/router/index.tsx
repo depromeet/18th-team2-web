d857950795e41011120d7775621b593e9018ef0d
xxxjinn <tlsguswls319@naver.com>
2026-04-26 03:35:14 +0900

    feat: 온보딩페이지 라우터 변경

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,10 @@
 import { createBrowserRouter } from 'react-router-dom';
 
-import HomePage from '@/pages/HomePage';
+import { OnboardingPage } from '@/pages/OnboardingPage';
 
 export const router = createBrowserRouter([
   {
-    path: '/',
-    element: <HomePage />,
+    path: '/onboarding',
+    element: <OnboardingPage />,
   },
 ]);
