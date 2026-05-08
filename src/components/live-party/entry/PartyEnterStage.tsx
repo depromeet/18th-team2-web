e51a500e3fafcaf30932884b607c1b1d88ab3e1e
xxxjinn <tlsguswls319@naver.com>
2026-05-08 12:36:31 +0900

    feat(party): 주인공 등장 img 추가

--- src/components/live-party/entry/PartyEnterStage.tsx
+++ src/components/live-party/entry/PartyEnterStage.tsx
@@ -1,3 +1,4 @@
+import DefaultHost from '@/assets/images/live-party/default-host.svg?react';
 import whiteGradientBig from '@/assets/images/live-party/white-gradient-big.png';
 import { T3 } from '@/components/ui/Typography';
 
@@ -5,18 +6,15 @@ export function PartyEnterStage() {
   return (
     <div className="absolute inset-0 z-10">
       <img src={whiteGradientBig} className="absolute top-0 left-0 w-full" />
-      <div className="flex h-full flex-col items-center justify-center">
-        {/* TODO: 캐릭터 이미지 */}
-        <div className="party-enter-character h-[188px] w-[160px] bg-sky-400" />
-
-        <T3 className="mt-25 text-center text-white">
-          오늘의 주인공
-          <br />
-          현진님이
-          <br />
-          등장했어요!
-        </T3>
-      </div>
+      {/* 주최자 - 프리런칭을 위한 하드코딩 */}
+      <DefaultHost className="party-enter-character absolute top-[45%] left-1/2 -translate-x-1/2 -translate-y-1/2" />
+      <T3 className="absolute right-0 bottom-32 left-0 text-center text-white">
+        오늘의 주인공
+        <br />
+        현진님이
+        <br />
+        등장했어요!
+      </T3>
     </div>
   );
 }
