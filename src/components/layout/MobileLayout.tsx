871226b65687c7bf29f29f6893cc4232c6c1301d
xxxjinn <tlsguswls319@naver.com>
2026-05-14 00:05:46 +0900

    refactor(layout): 레이아웃 border-x 삭제

--- src/components/layout/MobileLayout.tsx
+++ src/components/layout/MobileLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet, ScrollRestoration } from 'react-router-dom';
 export function MobileLayout() {
   return (
     <div className="min-h-screen bg-white">
-      <div className="border-grey-100 mx-auto flex min-h-screen w-full max-w-150 flex-col border-x bg-white">
+      <div className="mx-auto flex min-h-screen w-full max-w-150 flex-col bg-white">
         <Outlet />
       </div>
       <ScrollRestoration />
