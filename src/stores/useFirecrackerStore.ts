2f03c3a3ae16bafb416fe15062cf1b295696bf47
xxxjinn <tlsguswls319@naver.com>
2026-05-20 01:29:17 +0900

    feat(party): 폭죽 store에 devtools 추가

--- src/stores/useFirecrackerStore.ts
+++ src/stores/useFirecrackerStore.ts
@@ -1,15 +1,21 @@
 import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
 
 interface FirecrackerState {
   firecrackerId: number;
   fire: () => void;
 }
 
-export const useFirecrackerStore = create<FirecrackerState>((set) => ({
-  firecrackerId: 0,
+export const useFirecrackerStore = create<FirecrackerState>()(
+  devtools(
+    (set) => ({
+      firecrackerId: 0,
 
-  fire: () =>
-    set({
-      firecrackerId: Date.now(),
+      fire: () =>
+        set({
+          firecrackerId: Date.now(),
+        }),
     }),
-}));
+    { name: 'FirecrackerStore' },
+  ),
+);
