eacccb5048ef4d2040e5e06a52eb1319632843bc
xxxjinn <tlsguswls319@naver.com>
2026-05-20 00:58:08 +0900

    feat(party): 주인공 등장 시 파티 시작 버튼 추가

--- src/components/live-party/entry/PartyEntryStage.tsx
+++ src/components/live-party/entry/PartyEntryStage.tsx
@@ -1,20 +1,39 @@
 import DefaultHost from '@/assets/images/live-party/default-host.svg?react';
 import whiteGradientBig from '@/assets/images/live-party/white-gradient-big.png';
+
+import { Button } from '@/components/ui/Button';
 import { T3 } from '@/components/ui/Typography';
 
-export function PartyEntryStage() {
+interface PartyEntryStageProps {
+  onComplete?: () => void;
+}
+
+export function PartyEntryStage({ onComplete }: PartyEntryStageProps) {
   return (
-    <div className="absolute inset-0 z-10">
-      <img src={whiteGradientBig} className="absolute top-0 left-0 w-full" />
-      {/* 주최자 - 프리런칭을 위한 하드코딩 */}
-      <DefaultHost className="party-enter-character absolute top-[45%] left-1/2 -translate-x-1/2 -translate-y-1/2" />
-      <T3 className="absolute right-0 bottom-32 left-0 text-center text-white">
-        오늘의 주인공
-        <br />
-        이라님이
-        <br />
-        등장했어요!
-      </T3>
+    <div className="relative flex h-full flex-col overflow-hidden">
+      <img
+        src={whiteGradientBig}
+        alt=""
+        aria-hidden
+        className="pointer-events-none absolute top-0 left-0 w-full"
+      />
+
+      <div className="flex flex-1 flex-col items-center justify-center gap-20 px-2">
+        <DefaultHost className="party-enter-character w-44" />
+        <T3 className="text-center text-white">
+          오늘의 주인공
+          <br />
+          이라님이
+          <br />
+          등장했어요!
+        </T3>
+      </div>
+      {/** TODO: 디자인 아직 나오지 않음 */}
+      <div className="z-1 px-4 pb-8">
+        <Button onClick={onComplete} className="">
+          파티 시작하기
+        </Button>
+      </div>
     </div>
   );
 }
