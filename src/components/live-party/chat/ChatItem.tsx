7f1e0ff4771b371377900cd51023e020cc321949
xxxjinn <tlsguswls319@naver.com>
2026-05-22 15:43:38 +0900

    feat(party): 채팅방 캐릭터 프로필 border 추가

--- src/components/live-party/chat/ChatItem.tsx
+++ src/components/live-party/chat/ChatItem.tsx
@@ -1,4 +1,5 @@
 import { B2, L2 } from '@/components/ui/Typography';
+
 import StarIconSvg from '@/assets/images/live-party/star.svg?react';
 import PartyHatSvg from '@/assets/images/live-party/party-hat.svg?react';
 
@@ -10,24 +11,33 @@ interface ChatItemProps {
 }
 
 export function ChatItem({ name, profileImage, text, senderRole }: ChatItemProps) {
+  const isCelebrant = senderRole === 'CELEBRANT';
+
   return (
     <div className="flex items-start gap-3">
-      <div className="relative inline-block shrink-0">
-        <img src={profileImage} alt={name} className="h-8 w-8 rounded-full object-cover" />
-        {senderRole === 'CELEBRANT' && (
-          <PartyHatSvg className="absolute top-0 right-0 z-10 w-5 translate-x-[35%] -translate-y-[50%] rotate-25" />
+      <div className="relative shrink-0">
+        <div
+          className={`rounded-full p-[2px] ${
+            isCelebrant ? 'bg-linear-to-b from-[#E8B55D] to-[#F0409B]' : 'bg-white'
+          }`}
+        >
+          <img src={profileImage} alt={name} className="h-8 w-8 rounded-full object-cover" />
+        </div>
+
+        {isCelebrant && (
+          <PartyHatSvg className="absolute top-0 right-0 z-10 w-5 translate-x-[35%] -translate-y-[50%] rotate-30" />
         )}
       </div>
+
       <div className="flex flex-col">
-        <span className="flex">
-          <L2
-            as="p"
-            className={`${senderRole === 'PARTICIPANT' ? 'text-white' : 'text-yellow-300'}`}
-          >
+        <span className="flex items-center gap-1">
+          <L2 as="p" className={isCelebrant ? 'text-yellow-300' : 'text-white'}>
             {name}
           </L2>
-          {senderRole === 'CELEBRANT' && <StarIconSvg />}
+
+          {isCelebrant && <StarIconSvg />}
         </span>
+
         <B2 as="p" className="font-normal text-white">
           {text}
         </B2>
