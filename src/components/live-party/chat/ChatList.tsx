11587d968ee248bd3d80f3a32bb9a041ad3fecae
xxxjinn <tlsguswls319@naver.com>
2026-05-22 15:57:54 +0900

    feat(party): 퇴장 알림 추가

--- src/components/live-party/chat/ChatList.tsx
+++ src/components/live-party/chat/ChatList.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useRef } from 'react';
+
 import { ChatItem } from '@/components/live-party/chat/ChatItem';
-import { EntryNoticeText } from '@/components/live-party/chat/EntryNoticeText';
+import { EntryExitNoticeText } from '@/components/live-party/chat/EntryExitNoticeText';
+
 import { type ChatListItem } from '@/hooks/live-party/useChatBottomSheet';
 
 interface ChatListProps {
@@ -12,18 +14,25 @@ export function ChatList({ messages, isExpanded }: ChatListProps) {
   const bottomRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    bottomRef.current?.scrollIntoView({ behavior: 'smooth' });
+    bottomRef.current?.scrollIntoView({
+      behavior: 'smooth',
+    });
   }, [messages]);
 
   return (
     <div className="share-scroll-hide relative mb-5 flex-1 space-y-4 overflow-y-auto pt-4">
       <div
-        className={`pointer-events-none absolute top-0 right-0 left-0 h-10 ${isExpanded ? 'bg-none' : 'bg-linear-to-b from-[#26295D] to-transparent'}`}
+        className={`pointer-events-none absolute top-0 right-0 left-0 h-10 ${
+          isExpanded ? 'bg-none' : 'bg-linear-to-b from-[#26295D] to-transparent'
+        }`}
       />
-      {messages.map((item) =>
-        item.type === 'entry' ? (
-          <EntryNoticeText key={item.id} userName={item.userName} />
-        ) : (
+
+      {messages.map((item) => {
+        if (item.type === 'entry' || item.type === 'exit') {
+          return <EntryExitNoticeText key={item.id} userName={item.userName} type={item.type} />;
+        }
+
+        return (
           <ChatItem
             key={item.id}
             name={item.user.name}
@@ -31,8 +40,9 @@ export function ChatList({ messages, isExpanded }: ChatListProps) {
             text={item.text}
             senderRole={item.user.senderRole}
           />
-        ),
-      )}
+        );
+      })}
+
       <div ref={bottomRef} />
     </div>
   );
