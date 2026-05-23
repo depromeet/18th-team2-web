0ea48860b3455c710da40784d101672bcc1e7bc7
xxxjinn <tlsguswls319@naver.com>
2026-05-24 00:53:14 +0900

    feat(party): 이름 수정 시 api 409 에러 대응

--- src/pages/PartyEnterPage.tsx
+++ src/pages/PartyEnterPage.tsx
@@ -17,6 +17,7 @@ export default function PartyEnterPage() {
     inputValue,
     isNicknameEditable,
     inputMessage,
+    isInputError,
     selectedCharacterId,
     handleChangeNickname,
     handleSelectCharacter,
@@ -34,6 +35,7 @@ export default function PartyEnterPage() {
               value={inputValue}
               placeholder="파티에 등장할 닉네임을 입력해 주세요."
               disabled={!isNicknameEditable}
+              error={isInputError}
               message={inputMessage}
               onChange={handleChangeNickname}
             />
