84506c0ccc83086724c1b824bdba2ca8c4291ea3
xxxjinn <tlsguswls319@naver.com>
2026-06-05 23:39:28 +0900

    refactor(party): 불필요한 mock data 삭제

--- src/services/party-enter.ts
+++ src/services/party-enter.ts
@@ -10,24 +10,6 @@ export interface Participant {
   imageUrl: string;
 }
 
-export const MOCK_PARTICIPANTS: Participant[] = [
-  {
-    id: 1,
-    nickname: '하파린',
-    imageUrl: 'https://placehold.co/40x40',
-  },
-  {
-    id: 2,
-    nickname: '소다',
-    imageUrl: 'https://placehold.co/40x40',
-  },
-  {
-    id: 3,
-    nickname: '민트',
-    imageUrl: 'https://placehold.co/40x40',
-  },
-];
-
 export type ParticipantRealtimeProfile = components['schemas']['ParticipantRealtimeProfileResult'];
 export type UpsertRealtimeProfileRequest =
   components['schemas']['UpsertParticipantRealtimeProfileRequest'];
