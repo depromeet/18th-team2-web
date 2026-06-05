3a9411b26a98c9332ca622ae50e6522720d9ad20
xxxjinn <tlsguswls319@naver.com>
2026-06-05 23:58:43 +0900

    feat(party): 실시간 파티 헤더의 건너뛰기 버튼 삭제

--- src/components/live-party/LivePartyHeader.tsx
+++ src/components/live-party/LivePartyHeader.tsx
@@ -1,12 +1,9 @@
 import { CloseIcon } from '@/components/ui/icons/CloseIcon';
-import { Button } from '@/components/ui/Button';
 import { LIVE_PARTY_STEP, type PartyStep } from '@/constants/live-party';
 import MusicPlayIconSvg from '@/assets/images/live-party/music-play.svg?react';
 import MusicMutedIconSvg from '@/assets/images/live-party/music-muted.svg?react';
 
-//임시? 추후 공통 컴포넌트로 교체 후 삭제될 가능성 있음
 interface LivePartyHeaderProps {
-  onNextStep: () => void;
   onExitClick: () => void;
   step: PartyStep;
   handleToggleMute: () => void;
@@ -14,25 +11,19 @@ interface LivePartyHeaderProps {
 }
 
 export function LivePartyHeader({
-  onNextStep,
   onExitClick,
   step,
   handleToggleMute,
   musicIsMuted,
 }: LivePartyHeaderProps) {
   return (
-    <header className="absolute top-0 right-0 left-0 z-30 flex items-center justify-between p-4">
+    <header className="absolute top-0 right-0 left-0 z-30 flex items-center p-4">
       {step !== LIVE_PARTY_STEP.ENTRY && (
         <button onClick={handleToggleMute}>
           {musicIsMuted ? <MusicMutedIconSvg /> : <MusicPlayIconSvg />}
         </button>
       )}
-
-      {/* 프리런칭 데이를 위한 임시 스텝 버튼 */}
-      <Button size="sm" onClick={onNextStep}>
-        다음 스텝
-      </Button>
-      <button onClick={onExitClick} aria-label="파티 나가기">
+      <button onClick={onExitClick} aria-label="파티 나가기" className="ml-auto">
         <CloseIcon className="text-white" />
       </button>
     </header>
