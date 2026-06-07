4a7c5a7b09d9d9033c945510cc485a8154f64c0d
xxxjinn <tlsguswls319@naver.com>
2026-06-07 21:54:10 +0900

    feat(party): 다음 스텝 버튼 삭제

--- src/components/live-party/LivePartyHeader.tsx
+++ src/components/live-party/LivePartyHeader.tsx
@@ -1,12 +1,9 @@
 import { CloseIcon } from '@/components/ui/icons/CloseIcon';
-import { Button } from '@/components/ui/Button';
 import { LIVE_PARTY_STEP, type PartyStep } from '@/constants/live-party';
 import MusicPlayIconSvg from '@/assets/images/live-party/music-play.svg?react';
 import MusicMutedIconSvg from '@/assets/images/live-party/music-muted.svg?react';
 
-//임시? 추후 공통 컴포넌트로 교체 후 삭제될 가능성 있음
 interface LivePartyHeaderProps {
-  onNextStep: () => void;
   onExitClick: () => void;
   step: PartyStep;
   handleToggleMute: () => void;
@@ -14,7 +11,6 @@ interface LivePartyHeaderProps {
 }
 
 export function LivePartyHeader({
-  onNextStep,
   onExitClick,
   step,
   handleToggleMute,
@@ -27,12 +23,7 @@ export function LivePartyHeader({
           {musicIsMuted ? <MusicMutedIconSvg /> : <MusicPlayIconSvg />}
         </button>
       )}
-
-      {/* 프리런칭 데이를 위한 임시 스텝 버튼 */}
-      <Button size="sm" onClick={onNextStep}>
-        다음 스텝
-      </Button>
-      <button onClick={onExitClick} aria-label="파티 나가기">
+      <button onClick={onExitClick} aria-label="파티 나가기" className="ml-auto">
         <CloseIcon className="text-white" />
       </button>
     </header>
