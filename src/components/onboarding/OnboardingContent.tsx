a3ebcee35cc6f0b152811b234513c83f7f45aa9e
xxxjinn <tlsguswls319@naver.com>
2026-06-09 00:06:08 +0900

    feat(onboarding): 온보딩 텍스트 변경 및 수정 이미지 적용

--- src/components/onboarding/OnboardingContent.tsx
+++ src/components/onboarding/OnboardingContent.tsx
@@ -1,15 +1,22 @@
-import { H1 } from '@/components/ui/Typography';
+import { H1, H3 } from '@/components/ui/Typography';
 
 interface OnboardingContentProps {
   imageSrc: string;
-  text: string;
+  text: { bigText: string; smallText: string };
 }
 
 export function OnboardingContent({ imageSrc, text }: OnboardingContentProps) {
   return (
     <div className="flex flex-col items-center justify-center gap-9">
-      <img src={imageSrc} alt="온보딩 이미지" />
-      <H1 className="text-center whitespace-pre-line text-white">{text}</H1>
+      <img src={imageSrc} alt="온보딩 이미지" className="h-80 w-[343px]" />
+      <div className="flex flex-col gap-1">
+        <H1 className="text-center whitespace-pre-line text-white" as="p">
+          {text.bigText}
+        </H1>
+        <H3 className="text-center font-semibold text-white" as="p">
+          {text.smallText}
+        </H3>
+      </div>
     </div>
   );
 }
--- src/constants/onboarding.ts
+++ src/constants/onboarding.ts
@@ -1,17 +1,28 @@
 import onboardingFirstImage from '@/assets/images/onboarding/onboarding-first.png';
+import onboardingSecondImage from '@/assets/images/onboarding/onboarding-second.png';
+import onboardingThirdImage from '@/assets/images/onboarding/onboarding-third.png';
 
 export const ONBOARDING_CONTENTS = [
   //TODO: 2,3번째 이미지 추후 디자인팀에서 추가 예정
   {
     imageSrc: onboardingFirstImage,
-    text: '그냥 보내기 아쉬운 생일, \n온라인 생일파티 어때요?',
+    text: {
+      bigText: '생일, 그냥 넘기기 아쉬웠다면',
+      smallText: '온라인으로 모여 짧게 축하받아보세요',
+    },
   },
   {
-    imageSrc: onboardingFirstImage,
-    text: '실제 생일의 경험을 \n빠르고 간단하게 즐길 수 있어요',
+    imageSrc: onboardingSecondImage,
+    text: {
+      bigText: '빠르고 가볍게, 생일답게',
+      smallText: '함께 입장해 축하를 나눌 수 있어요',
+    },
   },
   {
-    imageSrc: onboardingFirstImage,
-    text: '파티가 부담스럽다면 \n롤링페이퍼만 남길수도 있어요',
+    imageSrc: onboardingThirdImage,
+    text: {
+      bigText: '함께 못 모여도 마음은 남길 수 있어요',
+      smallText: '롤링페이퍼로 마음을 받고 남길 수 있어요',
+    },
   },
 ];
