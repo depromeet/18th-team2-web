2e7841e47a2752634fccf23e9d20e5dfbdd66f13
xxxjinn <tlsguswls319@naver.com>
2026-06-11 00:29:38 +0900

    feat(loading): opacity 조정

--- src/components/ui/Loading.tsx
+++ src/components/ui/Loading.tsx
@@ -2,7 +2,7 @@ import { B1 } from './Typography';
 
 export function Loading() {
   return (
-    <div className="fixed inset-0 z-200 flex flex-col items-center justify-center gap-2 bg-black/40">
+    <div className="fixed inset-0 z-200 flex flex-col items-center justify-center gap-2 bg-black/50">
       {/** TODO: 로딩 이미지 추가 */}
       <div className="h-38.75 w-38.75 bg-amber-200">이미지 자리</div>
       <B1 as="p" className="text-grey-30">
