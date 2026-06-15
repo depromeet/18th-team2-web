aa0cd48900cf12e3a3c5e47850f26db21a038d12
HyunJin <tlsguswls319@naver.com>
2026-06-15 23:57:40 +0900

    fix(image): 이미지 파일 변경 (#187)
    * feat(onboarding): 온보딩 배경색 수정
    
    * refactor(onboarding): 불필요한 주석 삭제
    
    * fix(party): music icon png로 변경
    
    * fix(party): header 음악 버튼 png로 적용
    
    * fix(party): translateZ(0) 추가
    
    * fix(onboarding): 온보딩 이미지 파일 변경 및 적용
    
    * feat(onboarding): 이미지에 블러 추가

--- src/components/onboarding/OnboardingContent.tsx
+++ src/components/onboarding/OnboardingContent.tsx
@@ -8,7 +8,11 @@ interface OnboardingContentProps {
 export function OnboardingContent({ imageSrc, text }: OnboardingContentProps) {
   return (
     <div className="flex flex-col items-center justify-center gap-9">
-      <img src={imageSrc} alt="온보딩 이미지" className="h-80 w-[343px]" />
+      <img
+        src={imageSrc}
+        alt="온보딩 이미지"
+        className="h-80 w-[343px] mask-[linear-gradient(to_right,transparent_0%,black_15%,black_95%,transparent_100%),linear-gradient(to_bottom,black_95%,transparent_100%)] mask-intersect"
+      />
       <div className="flex flex-col gap-1">
         <H1 className="text-center whitespace-pre-line text-white" as="p">
           {text.bigText}
