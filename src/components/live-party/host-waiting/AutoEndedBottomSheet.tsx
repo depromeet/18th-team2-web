8ac9fe9719426eed202f717537746b904184bdd8
xxxjinn <tlsguswls319@naver.com>
2026-09-01 00:07:13 +0900

    refactor: 바텀 시트 회색 바 제거

--- src/components/live-party/host-waiting/AutoEndedBottomSheet.tsx
+++ src/components/live-party/host-waiting/AutoEndedBottomSheet.tsx
@@ -16,9 +16,6 @@ export function AutoEndedBottomSheet({
   return (
     <div className="fixed inset-y-0 left-1/2 z-50 w-full max-w-150 -translate-x-1/2 bg-black/70">
       <section className="absolute bottom-8 left-1/2 flex w-[calc(100%-24px)] -translate-x-1/2 flex-col overflow-hidden rounded-2xl bg-white">
-        <div className="flex h-4 items-end justify-center">
-          <span className="bg-grey-100 h-1 w-12 rounded-full" />
-        </div>
         <header className="flex items-start justify-between px-5 pt-5 pb-2.5">
           <H2 className="text-black">
             파티가 자동 종료되었어요 🥲
