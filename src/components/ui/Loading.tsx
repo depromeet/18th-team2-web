d75e39ec836b0fb8674d54ac2adb48d52aa3ee46
xxxjinn <tlsguswls319@naver.com>
2026-09-07 20:22:01 +0900

    refactor: 로딩 컴포넌트 수정

--- src/components/ui/Loading.tsx
+++ src/components/ui/Loading.tsx
@@ -1,30 +1,45 @@
 import Lottie from 'lottie-react';
 
-import loadingAnimation from '@/assets/images/common/loading-character.json';
+import characterLoadingAnimation from '@/assets/images/common/loading-character.json';
+import { ThreeDotsIcon } from './icons/ThreeDotsIcon';
 import { B1 } from './Typography';
 
 interface LoadingProps {
-  variant?: 'overlay' | 'white';
+  variant?: 'overlay' | 'white' | 'black';
 }
 
 export function Loading({ variant = 'overlay' }: LoadingProps) {
   if (variant === 'white') {
     return (
       <div className="flex h-full min-h-dvh flex-col items-center justify-center gap-2 bg-white">
-        <Lottie animationData={loadingAnimation} className="h-38.75 w-38.75" loop />
+        <Lottie animationData={characterLoadingAnimation} className="h-38.75 w-38.75" loop />
         <B1 as="p" className="text-grey-400">
           로딩중이에요
         </B1>
+        <ThreeDotsIcon />
+      </div>
+    );
+  }
+
+  if (variant === 'black') {
+    return (
+      <div className="flex h-full min-h-dvh flex-col items-center justify-center gap-2 bg-black/70">
+        <Lottie animationData={characterLoadingAnimation} className="h-38.75 w-38.75" loop />
+        <B1 as="p" className="text-grey-400">
+          로딩중이에요
+        </B1>
+        <ThreeDotsIcon />
       </div>
     );
   }
 
   return (
     <div className="fixed inset-0 z-200 flex flex-col items-center justify-center gap-2 bg-black/50">
-      <Lottie animationData={loadingAnimation} className="h-38.75 w-38.75" loop />
+      <Lottie animationData={characterLoadingAnimation} className="h-38.75 w-38.75" loop />
       <B1 as="p" className="text-grey-30">
         로딩중이에요
       </B1>
+      <ThreeDotsIcon />
     </div>
   );
 }
